133db0bac308f0c25457f0f8fe9ef141e00253a9
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-17 23:24:35 -0300

    Create GetMarkets method

--- FTX.Api/FTX.Api/FTX.cs
+++ FTX.Api/FTX.Api/FTX.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using FTX.Api.Entities;
+using FTX.Api.Services;
+using System;
+using FTX.Api.Exceptions;
 
 namespace FTX.Api
 {
     public class FTX
     {
+        private readonly IFTXClient _ftxClient;
+        public FTX(IFTXClient ftxClient)
+        {
+            _ftxClient = ftxClient;
+        }
+
+        public async Task<List<Market>> GetMarketsAsync()
+        {
+            try
+            {
+                string path = "/markets";
+                List<Market> markets = await _ftxClient.GetAsync<List<Market>>(path);
+                return markets;
+            }
+            catch (Exception ex)
+            {
+                throw new FTXException("Your message here", ex);
+            }
+        }
     }
 }
