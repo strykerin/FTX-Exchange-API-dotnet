8ac68ce594a469899607e656afdc6803b433ba79
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-12-23 20:04:16 -0300

    Create IFTX interface

---
+++ FTX.Api/FTX.Api/IFTX.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using FTX.Api.Entities;
+
+namespace FTX.Api
+{
+    public interface IFTX
+    {
+        Task<List<Market>> GetMarketsAsync();
+    }
+}
