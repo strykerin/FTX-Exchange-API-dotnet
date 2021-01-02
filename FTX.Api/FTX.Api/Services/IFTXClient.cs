612f157fc2cfd733b1aa54cfec0e14ea56318b98
Fabio Costa <dacosta.pereirafabio@gmail.com>
2021-01-02 15:23:27 -0300

    add non authenticated call to client interface

--- FTX.Api/FTX.Api/Services/IFTXClient.cs
+++ FTX.Api/FTX.Api/Services/IFTXClient.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using FTX.Api.Entities;
+using System.Threading.Tasks;
 
 namespace FTX.Api.Services
 {
     public interface IFTXClient
     {
-        Task<T> GetAsync<T>(string path);
+        Task<Response<T>> GetAsync<T>(string path);
+        Task<Response<T>> GetAuthenticatedAsync<T>(string path);
     }
 }
